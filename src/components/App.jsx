6b4ebb94968cffd946c63ab651babf2c117a48cf
miiky976 <mruiz3445@gmail.com>
2022-07-25 21:31:29 -0500

    gh-pages Solucion al router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
 import { Cabeza } from "./Cabeza";
 import {Registrar} from "./Registrar";
 import { Comprar } from "./Comprar";
-import { BrowserRouter,Routes ,Route, Link } from "react-router-dom";
+import { HashRouter,Routes ,Route, Link } from "react-router-dom";
 import { Inicio } from "./Inicio";
 export const App = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Cabeza titulo='CarChain'></Cabeza>
             <Routes>
                 <Route path="/" element={<Inicio/>}/>
                 <Route path="/Registrar" element={<Registrar/>}/>
                 <Route path="/Comprar" element={<Comprar/>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
